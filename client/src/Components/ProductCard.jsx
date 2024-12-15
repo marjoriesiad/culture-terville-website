1afbe41100f04cb0f2f966a681c17c7f04cc2356
Marjorie SIAD <marjoriesiad@gmail.com>
2024-12-15 01:04:59 +0100

    create backend and install packages

--- client/src/Components/ProductCard.jsx
+++ client/src/Components/ProductCard.jsx
@@ -5,7 +5,7 @@ const ProductCard = () => {
   return (
     <div className="relative">
       <img src="http://via.placeholder.com/150" alt="" className="w-full h-full object-cover" />
-      <div className="opacity-0 hover:opacity-100 absolute inset-0 bg-white bg-opacity-60 flex flex-col justify-between p-2 py-3 text-center cursor-pointer transition duration-300 ease-in-out">
+      <div className="opacity-0 hover:opacity-100 absolute inset-0 bg-white bg-opacity-60 flex first:flex-col justify-between p-2 py-3 text-center cursor-pointer transition duration-300 ease-in-out">
         <p className="text-2xl font-semibold">Pain</p>
         <p className="text-main-red font-semibold">2.20€</p>
         <div className="flex justify-between items-center">
