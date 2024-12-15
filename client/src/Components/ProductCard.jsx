6d7af90665f01c6d17f1456adeb650edb618ad08
Marjorie SIAD <marjoriesiad@gmail.com>
2024-12-15 01:49:41 +0100

    create traiteur page with products from db

--- client/src/Components/ProductCard.jsx
+++ client/src/Components/ProductCard.jsx
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
 import CartSvg from "../assets/CartSvg";
 
-const ProductCard = () => {
+const ProductCard = (props) => {
+  const formattedPrice = parseFloat(props.price).toFixed(2)
+
   return (
-    <div className="relative">
-      <img src="http://via.placeholder.com/150" alt="" className="w-full h-full object-cover" />
-      <div className="opacity-0 hover:opacity-100 absolute inset-0 bg-white bg-opacity-60 flex flex-col justify-between p-2 py-3 text-center cursor-pointer transition duration-300 ease-in-out">
-        <p className="text-2xl font-semibold">Pain</p>
-        <p className="text-main-red font-semibold">2.20€</p>
-        <div className="flex justify-between items-center">
-          <Link to="/pain/:slug" className="">Voir</Link>
+    <div className="relative h-32">
+      <img src={props.src} alt="" className="w-full h-full object-fill" />
+      <div className="opacity-0 hover:opacity-100 absolute inset-0 bg-white bg-opacity-70 p-2 flex flex-col justify-between text-center cursor-pointer transition duration-300 ease-in-out">
+        <h3 className="text-xl font-semibold w-32 mx-auto">{props.name}</h3>
+        <p className="text-main-red font-semibold">{formattedPrice}€</p>
+        <div className="flex justify-between items-center px-2">
+          <div className="flex items-center justify-center">
+            <Link to="/pain/:slug" className="font-medium">{`Voir >`}</Link>
+
+          </div>
+          
           <CartSvg class="w-6" />
         </div>
       </div>
--- client/src/Routes/TraiteurPage.jsx
+++ client/src/Routes/TraiteurPage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import axios from "axios";
+import ProductCard from "../Components/ProductCard";
 
 const TraiteurPage = () => {
-  return (
-    <div>TraiteurPage</div>
-  )
-}
+  const [products, setProducts] = React.useState([]);
+  useEffect(() => {
+    axios
+      .get(import.meta.env.VITE_API_URL + "/traiteur")
+      .then((res) => setProducts(res.data))
+      .catch((err) => console.log(err));
+  }, []);
 
-export default TraiteurPage
+  return (
+    <div className="flex justify-center mt-8">
+      <div className="grid grid-cols-3 gap-3">
+        {products.map((product) => (
+          <ProductCard key={product.id} src={product.image} name={product.name} price={product.price} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TraiteurPage;
